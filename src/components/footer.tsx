cefafc445f7bdc358c563af63cf01cf2aa24dbe8
Franklin Jara <franklinjaradev@gmail.com>
2022-03-25 05:34:04 -0400

    chore: footer changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center font-extrabold py-4">
+    <footer className="flex justify-center font-extrabold py-4 text-xl">
       <div>Copyright &copy; 2022 - Sprix</div>
     </footer>
   )
