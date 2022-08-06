d58ce0a6e9f7eb22573e5d18be177b4ed551aba9
Franklin Jara <franklinjaradev@gmail.com>
2022-08-06 01:14:27 -0400

    :package: Adding react pdf
    Setup for react pdf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
+import "../styles/globals.css"
+import type { AppType } from "next/dist/shared/lib/utils"
+import { PDFViewer } from "@react-pdf/renderer"
+import MyDocument from "./pdf"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <>
+      <Component {...pageProps}>
+        <PDFViewer>
+          <MyDocument />
+        </PDFViewer>
+      </Component>
+    </>
+  )
+}
 
-export default MyApp;
+export default MyApp
